c6ce218d7642fe26c750b1832f421029ce8cdedc
viktorlnz <37961018+viktorlnz@users.noreply.github.com>
2020-03-18 23:25:06 -0300

    Add files via upload

---
+++ geradorDeOfertas.py
@@ -0,0 +1,207 @@
+imoveis = ( #ATENÇÃO: COLE O SEU RESULTADO DO gerador de imoveis
+(1, 1, 1, 'AC', 'Travessa Aldeia', 151, 235, 655970.49, 0, 0, '2018-6-7'),
+(1, 2, 1, 'AC', 'Rua São José', 136, 246, 692276.67, 0, 0, '2018-2-21'),
+(1, 3, 2, 'AC', 'Rua Brasilândia', 275, 508, 941715.81, 0, 0, '2018-3-8'),
+(1, 4, 2, 'AC', 'Estrada Deputado José Rui da Silveira Lino', 284, 295, 881948.36, 0, 0, '2019-1-8'),
+(1, 5, 3, 'AC', 'Travessa Café', 213, 223, 414565.3, 0, 0, '2018-10-21'),
+(1, 6, 3, 'AC', 'Travessa Osvaldo Coelho', 232, 468, 289936.26, 0, 1, '2017-2-20'),
+(1, 7, 4, 'Al', 'Rua dos Lirios', 197, 271, 644825.47, 0, 1, '2019-2-4'),
+(1, 8, 4, 'Al', 'Travessa da Alegria', 99, 123, 352793.32, 0, 0, '2019-2-6'),
+(1, 9, 5, 'Al', 'Rua Projetada 302', 209, 386, 906925.47, 0, 0, '2018-11-1'),
+(1, 10, 5, 'Al', 'Rua I', 51, 84, 148959.66, 0, 0, '2018-10-24'),
+(1, 11, 6, 'Al', 'Rua da União', 93, 157, 645678.45, 0, 1, '2018-11-20'),
+(2, 12, 6, 'Al', 'Rua Gonçalves Dias', 136, 209, 671126.07, 0, 1, '2019-7-5'),
+(2, 13, 7, 'AP', 'Travessa Liberdade II', 227, 281, 493097.48, 0, 1, '2019-11-16'),
+(2, 14, 7, 'AP', 'Rua James Carlos', 51, 111, 506869.93, 0, 0, '2017-1-20'),
+(2, 15, 8, 'AP', 'Rua Sabiá', 145, 363, 919762.23, 0, 1, '2018-2-8'),
+(2, 16, 8, 'AP', 'Estrada Juarez Távora', 155, 310, 396317.3, 0, 0, '2018-1-25'),
+(2, 17, 9, 'AP', 'Rua José de Araújo', 53, 55, 634279.12, 0, 1, '2019-4-22'),
+(2, 18, 9, 'AP', 'Rua Tião Naturaza', 131, 192, 324975.56, 0, 1, '2019-10-10'),
+(2, 19, 10, 'AM', 'Rua Andiroba', 169, 343, 387941.73, 0, 0, '2019-1-4'),
+(2, 20, 10, 'AM', 'Travessa Avestruz', 197, 348, 423967.74, 0, 0, '2018-5-12'),
+(2, 21, 11, 'AM', 'Rua Marte', 107, 133, 793636.72, 0, 1, '2018-6-20'),
+(2, 22, 11, 'AM', 'Avenida Antônio da Rocha Viana', 95, 144, 605659.73, 0, 1, '2019-3-20'),
+(3, 23, 12, 'AM', 'Estrada São do Francisco', 194, 442, 734492.7, 0, 1, '2018-6-24'),
+(3, 24, 12, 'AM', 'Rua Estrela', 115, 131, 481882.05, 0, 0, '2018-1-27'),
+(3, 25, 13, 'BA', 'Travessa Verde', 206, 560, 692151.58, 0, 1, '2018-5-20'),
+(3, 26, 13, 'BA', 'Travessa Monte das Oliveiras', 106, 253, 287070.76, 0, 0, '2019-11-5'),
+(3, 27, 14, 'BA', 'Travessa Campo do Jordão', 212, 271, 408831.87, 0, 1, '2018-3-16'),
+(3, 28, 14, 'BA', 'Ramal da Zezé', 189, 192, 564462.63, 0, 1, '2019-2-23'),
+(3, 29, 15, 'BA', 'Rua Brigadeiro Damasceno', 205, 428, 649119.2, 0, 0, '2019-11-2'),
+(3, 30, 15, 'BA', 'Avenida Edmundo Pinto', 294, 332, 573395.56, 0, 0, '2018-4-9'),
+(3, 31, 16, 'CE', 'Via Parma', 208, 513, 162298.14, 0, 0, '2018-5-27'),
+(3, 32, 16, 'CE', 'Rua Waldemar Kruger', 154, 417, 720090.34, 0, 0, '2018-1-9'),
+(3, 33, 17, 'CE', 'Rua Waldemar Hesse', 98, 237, 514157.67, 0, 1, '2018-4-7'),
+(4, 34, 17, 'CE', 'Rua Abílio de Oliveira Borges', 84, 232, 223709.15, 0, 0, '2018-3-20'),
+(4, 35, 18, 'CE', 'Rua Tenente Lauro Gomes de Oliveira', 85, 228, 384301.79, 0, 1, '2018-11-25'),
+(4, 36, 18, 'CE', 'Avenida Hermann August Lepper 980', 89, 178, 152740.79, 0, 1, '2018-10-11'),
+(4, 37, 19, 'DF', 'Rua Maria Paulina Kluck', 194, 279, 147488.49, 0, 0, '2019-5-26'),
+(4, 38, 19, 'DF', 'Rua Portugal', 101, 229, 68499.29, 0, 0, '2017-5-18'),
+(4, 39, 20, 'DF', 'Rua Max Colin', 158, 175, 109848.93, 0, 0, '2018-8-4'),
+(4, 40, 20, 'DF', 'Rua Alfredo Colin', 50, 128, 846810.95, 0, 0, '2018-2-12'),
+(4, 41, 21, 'DF', 'Rua Américo Matrangola', 179, 422, 449348.64, 0, 1, '2018-4-27'),
+(4, 42, 21, 'DF', 'Rua Chede Dippe', 246, 541, 923263.85, 0, 0, '2017-8-7'),
+(4, 43, 22, 'ES', 'Rua Uruguaiana', 145, 352, 927577.43, 0, 0, '2018-11-23'),
+(4, 44, 22, 'ES', 'Rua Mossoró', 259, 626, 133979.98, 0, 0, '2018-8-25'),
+(5, 45, 23, 'ES', 'Rua Iracema Luckow', 175, 435, 903067.71, 0, 1, '2019-1-26'),
+(5, 46, 23, 'ES', 'Rodovia BR-101', 161, 244, 816490.67, 0, 1, '2018-9-7'),
+(5, 47, 24, 'ES', 'Rua Dom Gregório Warmeling', 241, 547, 349108.15, 0, 0, '2018-4-19'),
+(5, 48, 24, 'ES', 'Servidão Rubens Antonio Zimmermann', 60, 152, 131366.15, 0, 0, '2017-1-20'),
+(5, 49, 25, 'GO', 'Rua Natanael Amorim Vieira', 106, 152, 286375.51, 0, 1, '2017-6-12'),
+(5, 50, 25, 'GO', 'Rua Affonso Voss', 255, 300, 694363.34, 0, 1, '2018-1-1'),
+(5, 51, 26, 'GO', 'Rua Major Gercino', 259, 492, 447577.38, 0, 1, '2018-8-17'),
+(5, 52, 26, 'GO', 'Servidão Ione Rosa Correa Costa', 128, 206, 103868.27, 0, 1, '2017-9-26'),
+(5, 53, 27, 'GO', 'Servidão Ubatuba', 123, 170, 479149.71, 0, 0, '2018-2-11'),
+(5, 54, 27, 'GO', 'Rua Coronel Alyre Borges Carneiro', 86, 127, 760337.17, 0, 1, '2019-5-2'),
+(5, 55, 28, 'MA', 'Rua João Gomes de Oliveira', 199, 463, 932243.38, 0, 1, '2018-11-8'),
+(6, 56, 28, 'MA', 'Rua João Ramalho', 232, 433, 67219.92, 0, 0, '2018-1-14'),
+(6, 57, 29, 'MA', 'Rua Maria Elisabete Bagatoli', 99, 108, 130308.45, 0, 0, '2017-9-20'),
+(6, 58, 29, 'MA', 'Rua Maria Neves Niemeyer', 195, 495, 407961.86, 0, 0, '2018-3-23'),
+(6, 59, 30, 'MA', 'Rua Rudolpho Bruske', 293, 709, 653677.44, 0, 0, '2018-4-26'),
+(6, 60, 30, 'MA', 'Rua Martinho Rodolfo Heidemann', 208, 547, 573980.58, 0, 0, '2018-3-26'),
+(6, 61, 31, 'MT', 'Rua Luzia Russo Rodrigues', 137, 360, 912161.68, 0, 1, '2018-1-6'),
+(6, 62, 31, 'MT', 'Rua Iracema Bittencourt Ferraro', 283, 775, 965690.87, 0, 1, '2018-4-1'),
+(6, 63, 32, 'MT', 'Rua Café Catuaí', 129, 214, 771967.8, 0, 0, '2017-9-5'),
+(6, 64, 32, 'MT', 'Rua Dona Carlota', 76, 115, 194305.48, 0, 1, '2017-3-20'),
+(6, 65, 33, 'MT', 'Rua Café Cereja', 279, 700, 240222.81, 0, 0, '2018-2-26'),
+(6, 66, 33, 'MT', 'Rua Carmen Marcolla Pazini', 118, 153, 570252.94, 0, 1, '2017-6-24'),
+(7, 67, 34, 'MS', 'Avenida Isaltino Gonçalves Mendes', 150, 276, 237261.81, 0, 0, '2019-10-24'),
+(7, 68, 34, 'MS', 'Rua Elzira Bittencourt Guides', 267, 459, 413813.34, 0, 0, '2018-3-22'),
+(7, 69, 35, 'MS', 'Rua Guarapuava', 70, 94, 574432.66, 0, 1, '2019-1-5'),
+(7, 70, 35, 'MS', 'Praça Vicente Leporace', 219, 514, 214913.75, 0, 1, '2018-10-12'),
+(7, 71, 36, 'MS', 'Rua Quiri', 67, 106, 794258.83, 0, 0, '2018-7-13'),
+(7, 72, 36, 'MS', 'Rua do Guamirim', 266, 492, 525775.41, 0, 1, '2019-7-11'),
+(7, 73, 37, 'MG', 'Avenida José Ventura Pinto', 98, 147, 256878.91, 0, 0, '2017-3-5'),
+(7, 74, 37, 'MG', 'Rua Anita Garibaldi', 134, 324, 921064.85, 0, 0, '2019-9-25'),
+(7, 75, 38, 'MG', 'Rua São Vicente', 55, 153, 466149.56, 0, 0, '2018-8-3'),
+(7, 76, 38, 'MG', 'Rua Luiz Antônio Mayrink Goes', 258, 430, 179603.12, 0, 1, '2017-10-24'),
+(7, 77, 39, 'MG', 'Rua Florindo Boni', 141, 232, 832734.67, 0, 0, '2019-4-21'),
+(8, 78, 39, 'MG', 'Rua Raul Costa Júnior', 133, 300, 775507.31, 0, 0, '2018-6-13'),
+(8, 79, 40, 'PA', 'Rua Kirio Miyamura', 144, 292, 797062.1, 0, 1, '2019-1-3'),
+(8, 80, 40, 'PA', 'Rua Immo Vicentin', 113, 193, 331834.33, 0, 0, '2018-4-20'),
+(8, 81, 41, 'PA', 'Rua Pintassilgo', 224, 439, 933455.48, 0, 1, '2018-4-19'),
+(8, 82, 41, 'PA', 'Rua Ignacio Gonçalves', 190, 326, 262350.64, 0, 1, '2017-2-19'),
+(8, 83, 42, 'PA', 'Rua Acylino Augusto do Nascimento', 146, 256, 433891.54, 0, 0, '2017-1-23'),
+(8, 84, 42, 'PA', 'Rua Guararapes', 138, 299, 476239.48, 0, 0, '2018-11-19'),
+(8, 85, 43, 'PB', 'Praça George Graig Smith', 147, 296, 444066.11, 0, 1, '2018-2-3'),
+(8, 86, 43, 'PB', 'Rua Armando Forim', 97, 194, 407947.07, 0, 1, '2019-10-4'),
+(8, 87, 44, 'PB', 'Rua Heber Soares Vargas', 169, 469, 591051.35, 0, 0, '2017-4-13'),
+(8, 88, 44, 'PB', 'Rua Celso Kiyokatsu Mizuno', 178, 197, 260519.45, 0, 0, '2018-2-19'),
+(9, 89, 45, 'PB', 'Praça Pedro Caraco', 281, 517, 612695.71, 0, 0, '2018-2-7'),
+(9, 90, 45, 'PB', 'Rua Bauxita', 136, 296, 498363.81, 0, 1, '2017-4-1'),
+(9, 91, 46, 'PR', 'Rua Cidadania da Santa Felicidade', 202, 266, 875407.51, 0, 0, '2019-10-7'),
+(9, 92, 46, 'PR', 'Rua Bororós', 298, 572, 206036.96, 0, 1, '2018-1-7'),
+(9, 93, 47, 'PR', 'Rua Júlio Graf', 55, 71, 793182.5, 0, 0, '2018-3-15'),
+(9, 94, 47, 'PR', 'Rua Patrício Bagolin', 176, 394, 189651.29, 0, 1, '2018-1-22'),
+(9, 95, 48, 'PR', 'Rua Euclides Figueiredo', 248, 337, 179652.46, 0, 1, '2018-10-15'),
+(9, 96, 48, 'PR', 'Rua Major Eulálio Dudek', 190, 511, 653448.95, 0, 0, '2018-8-21'),
+(9, 97, 49, 'PE', 'Rua Rovigo', 90, 162, 231746.86, 0, 1, '2019-1-12'),
+(9, 98, 49, 'PE', 'Rua Ruy de Santis', 289, 514, 672857.86, 0, 1, '2019-8-20'),
+(9, 99, 50, 'PE', 'Rua Acelino Grande', 197, 527, 656045.33, 0, 1, '2018-2-20'),
+(10, 100, 50, 'PE', 'Rua Lídia Wosch', 212, 252, 641202.42, 0, 1, '2018-1-23'),
+(10, 101, 51, 'PE', 'Rua Campos Sales 623', 109, 137, 346139.23, 0, 0, '2017-6-11'),
+(10, 102, 51, 'PE', 'Rua Capitão Augusto do Rego Barros', 224, 611, 260412.86, 0, 0, '2018-9-27'),
+(10, 103, 52, 'PI', 'Rua Serafim Amur Ferreira do Amaral', 178, 338, 221757.59, 0, 0, '2018-2-11'),
+(10, 104, 52, 'PI', 'Rua Irene Túlio', 131, 272, 193023.19, 0, 0, '2019-1-8'),
+(10, 105, 53, 'PI', 'Rua Joaquim Amaral', 226, 244, 833021.5, 0, 0, '2017-5-4'),
+(10, 106, 53, 'PI', 'Rua Antônio Vieira dos Santos', 114, 222, 962604, 0, 0, '2019-9-18'),
+(10, 107, 54, 'PI', 'Rua Carlos Leinig Júnior', 222, 601, 864006.37, 0, 0, '2018-10-26'),
+(10, 108, 54, 'PI', 'Rua Betty Moreira Pacheco', 85, 113, 591604.67, 0, 1, '2017-9-3'),
+(10, 109, 55, 'RJ', 'Rua Manoel Valdomiro de Macedo', 177, 346, 147471, 0, 0, '2018-7-25'),
+(10, 110, 55, 'RJ', 'Rua Reinaldo Casagrande', 298, 634, 390756.16, 0, 1, '2018-9-19'),
+(11, 111, 56, 'RJ', 'Rua José Lass', 166, 234, 432958.23, 0, 0, '2019-7-6'),
+(11, 112, 56, 'RJ', 'Rua Werner Jehring', 265, 474, 147648.89, 0, 1, '2018-4-22'),
+(11, 113, 57, 'RJ', 'Rua Adair Gelenski', 183, 305, 640324.24, 0, 1, '2017-9-11'),
+(11, 114, 57, 'RJ', 'Rua Janusz Korczak', 107, 277, 194639.11, 0, 1, '2018-6-7'),
+(11, 115, 58, 'RN', 'Rua Estanislau Muchau', 292, 341, 701267.82, 0, 1, '2019-10-20'),
+(11, 116, 58, 'RN', 'Rua Doutor Plínio de Mattos Pessoa', 207, 449, 712920.67, 0, 1, '2018-1-26'),
+(11, 117, 59, 'RN', 'Rua Bento Azambuja', 259, 461, 576087.27, 0, 1, '2017-2-16'),
+(11, 118, 59, 'RN', 'Rua Brigadeiro Eduardo Gomes', 132, 227, 813772.17, 0, 1, '2018-4-21'),
+(11, 119, 60, 'RN', 'Rua Felipe Percegona', 247, 452, 585895.77, 0, 0, '2017-10-16'),
+(11, 120, 60, 'RN', 'Avenida Caviúna', 162, 184, 66156.31, 0, 0, '2019-1-15'),
+(11, 121, 61, 'RS', 'Rua Três Mil e Vinte e Sete', 99, 251, 207986.87, 0, 0, '2019-8-24'),
+(12, 122, 61, 'RS', 'Rua Nilson da Silva Santos', 97, 233, 793617.78, 0, 0, '2018-6-27'),
+(12, 123, 62, 'RS', 'Praça José Bartolomeu Greco', 281, 536, 646413.96, 0, 1, '2017-1-18'),
+(12, 124, 62, 'RS', 'Praça Diogo de Vasconcelos', 259, 463, 619806.13, 0, 0, '2018-1-22'),
+(12, 125, 63, 'RS', 'Rua Alexandre Stockler', 123, 258, 551816.12, 0, 1, '2019-7-15'),
+(12, 126, 63, 'RS', 'Rua Islândia', 85, 155, 209216.7, 0, 0, '2018-5-24'),
+(12, 127, 64, 'RO', 'Rua Padre Orlando Machado', 188, 475, 533341, 0, 1, '2019-10-8'),
+(12, 128, 64, 'RO', 'Rua Dez de Novembro', 259, 450, 686009.93, 0, 1, '2018-1-27'),
+(12, 129, 65, 'RO', 'Rua Brant Horta', 55, 134, 614641.82, 0, 0, '2018-2-27'),
+(12, 130, 65, 'RO', 'Praça Maria José Santiago', 247, 311, 875613.12, 0, 1, '2018-3-13'),
+(12, 131, 66, 'RO', 'Rua Guatambu', 156, 260, 561228.8, 0, 0, '2019-11-9'),
+(12, 132, 66, 'RO', 'Rua José Eduardo Moraes', 277, 620, 460028.18, 0, 0, '2018-1-22'),
+(13, 133, 67, 'RR', 'Rua Bananal', 219, 326, 165353.46, 0, 1, '2017-11-15'),
+(13, 134, 67, 'RR', 'Beco João Soares', 67, 112, 373343.06, 0, 1, '2018-11-1'),
+(13, 135, 68, 'RR', 'Rua Materlândia', 264, 266, 476884.15, 0, 0, '2017-2-16'),
+(13, 136, 68, 'RR', 'Rua Setecentos e Quinze', 175, 348, 329562.2, 0, 1, '2019-7-26'),
+(13, 137, 69, 'RR', 'Avenida Amazonas', 299, 424, 865598.45, 0, 0, '2018-3-11'),
+(13, 138, 69, 'RR', 'Rua Manila', 117, 127, 902940.49, 0, 0, '2018-5-23'),
+(13, 139, 70, 'SC', 'Rua José Bonifácio', 220, 451, 792153.57, 0, 0, '2017-6-20'),
+(13, 140, 70, 'SC', 'Rua Fernando Cândido de Souza', 182, 249, 299362.43, 0, 1, '2019-7-27'),
+(13, 141, 71, 'SC', 'Praça da Liberdade, s/n', 183, 499, 625039.98, 0, 0, '2018-5-12'),
+(13, 142, 71, 'SC', 'Praça Santa Rita', 155, 161, 463616.14, 0, 1, '2018-7-10'),
+(13, 143, 72, 'SC', 'Rua Maria Adelaide', 117, 238, 567658.9, 0, 0, '2017-4-9'),
+(14, 144, 72, 'SC', 'Rua Renato Luiz Lima', 143, 231, 627030.9, 0, 1, '2019-2-18'),
+(14, 145, 73, 'SP', 'Rua Pedra do Mar', 146, 159, 710279.18, 0, 0, '2019-4-13'),
+(14, 146, 73, 'SP', 'Rua José Brandão', 290, 374, 307014.94, 0, 0, '2017-8-17'),
+(14, 147, 74, 'SP', 'Rua Doutor Gonçalo de Moura', 82, 164, 886815.99, 0, 1, '2018-4-11'),
+(14, 148, 74, 'SP', 'Rua Doutor Roberto de Las Casas', 252, 362, 206490.1, 0, 1, '2019-4-14'),
+(14, 149, 75, 'SP', 'Avenida Neném Lara Rocha', 297, 389, 798004.25, 0, 0, '2019-10-10'),
+(14, 150, 75, 'SP', 'Estrada do Cercado', 274, 674, 946462.15, 0, 1, '2019-1-8'),
+(14, 151, 76, 'SE', 'Rodovia Francisco Thomaz dos Santos', 282, 282, 75050.14, 0, 1, '2019-1-18'),
+(14, 152, 76, 'SE', 'Servidão dos Curumins', 59, 70, 459419.93, 0, 1, '2017-8-11'),
+(14, 153, 77, 'SE', 'Travessa Júlia Mercedes da Conceição', 58, 115, 738290.89, 0, 1, '2017-3-3'),
+(14, 154, 77, 'SE', 'Rua Silva Jardim', 128, 198, 594576.57, 0, 1, '2017-7-10'),
+(15, 155, 78, 'SE', 'Rua Nossa Senhora do Rosário', 209, 351, 597929.18, 0, 1, '2018-10-7'),
+(15, 156, 78, 'SE', 'Rua Coronel Carlos Wenceslau Pacheco', 226, 361, 853239.31, 0, 0, '2018-3-26'),
+(15, 157, 79, 'TO', 'Servidão Bento Sales', 138, 193, 331274.32, 0, 1, '2018-6-4'),
+(15, 158, 79, 'TO', 'Servidão Ana Rita de Jesus', 131, 134, 550836.02, 0, 1, '2017-4-16'),
+(15, 159, 80, 'TO', 'Servidão Artino Rensi', 164, 398, 854332.39, 0, 0, '2017-8-1'),
+(15, 160, 80, 'TO', 'Rua Procurador Abelardo Gomes', 214, 526, 482870.47, 0, 0, '2019-1-11'),
+(15, 161, 81, 'TO', 'Rua Berlim', 159, 238, 240054.98, 0, 0, '2018-7-14'),
+(15, 162, 81, 'TO', 'Servidão Celso Domingos Martins', 231, 337, 744207.84, 0, 0, '2019-3-11')
+)
+
+import random
+
+ofertas = []
+qtd_compradores = 15
+ofertas_por_comprador = 162//qtd_compradores
+comprador_c = 1
+comprador_atual = 1
+
+
+for item in imoveis:
+	oferta = item[7] - item[7] % 100 - random.randint(1, 200) * 500
+	
+	data_split = item[10].split('-')
+	
+	mes = int(data_split[1])
+	ano = int(data_split[0])
+	
+	adicao_de_dia = random.randint(1, 28)
+	dia = int(data_split[2]) + adicao_de_dia
+	
+	if dia > 28: #O algoritmo não é complexo a ponto de identificar no exato qual o mês, logo supõe-se o risco de para no mês de Fevereiro
+		mes += 1
+		dia -= 28
+	
+	adicao_de_mes = random.randint(0,5) 
+	mes += adicao_de_mes
+	
+	if mes > 12:
+		ano += 1
+		mes -= 12
+	
+	
+	
+	data = str(ano)+'-'+str(mes)+'-'+str(dia)
+	ofertas.append((item[1], comprador_atual, oferta, data))
+	
+	comprador_c = comprador_c + 1 if comprador_c != ofertas_por_comprador else 1
+	
+	if comprador_c == 1:
+		comprador_atual += 1 if comprador_atual < qtd_compradores else 1
+	
+print(ofertas)
